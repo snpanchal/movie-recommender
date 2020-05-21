ca878acc41f0a476eba9c7f173077da36f26e8b0
Shyam Panchal <snpanchal44@gmail.com>
2020-05-20 21:38:37 -0600

    Add utils and sort similar movies

--- recommender.py
+++ recommender.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-from utils import combine_features
+import utils as utils
 
 # Read CSV dataset
 df = pd.read_csv("movie_dataset.csv")
@@ -15,7 +15,7 @@ features = ["keywords", "cast", "genres", "director"]
 for feature in features:
     df[feature] = df[feature].fillna("")
 
-df["combined_features"] = df.apply(combine_features, axis=1)
+df["combined_features"] = df.apply(utils.combine_features, features=features, axis=1)
 
 # Create count matrix for combined features column
 cv = CountVectorizer()
@@ -23,3 +23,16 @@ count_matrix = cv.fit_transform(df["combined_features"])
 
 # Calculate cosine similarity
 cosine_sim = cosine_similarity(count_matrix)
+movie_input = "Avatar"
+
+# Get index of movie from title
+movie_index = utils.get_index_from_title(movie_input, df)
+similar_movies = list(enumerate(cosine_sim[movie_index]))
+
+# Get list of similar movies in descending order
+sorted_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
+
+# Print titles of first 50 movies
+for i in range(50):
+    movie = sorted_movies[i]
+    print(utils.get_title_from_index(movie[0], df))
--- utils.py
+++ utils.py
@@ -4,5 +4,13 @@
 def combine_features(row, features):
     combined = ""
     for feature in features:
-        combined += (row[f] + " ")
+        combined += (row[feature] + " ")
     return combined
+
+
+def get_title_from_index(index, df):
+    return df[df.index == index]["title"].values[0]
+
+
+def get_index_from_title(title, df):
+    return df[df.title == title]["index"].values[0]
